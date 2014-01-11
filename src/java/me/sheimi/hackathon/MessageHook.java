f92de6a6c23a7f465451a14b07bb88b6e9ebb711
sheimi <sheimi.zhang@gmail.com>
2014-01-11 02:43:58 -0800

    fifo server

--- src/java/me/sheimi/hackathon/MessageHook.java
+++ src/java/me/sheimi/hackathon/MessageHook.java
@@ -1,30 +1,21 @@
 package me.sheimi.hackathon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collection;
+import java.io.OutputStreamWriter;
 
-import org.jivesoftware.openfire.MessageRouter;
-import org.jivesoftware.openfire.XMPPServer;
-import org.jivesoftware.openfire.muc.MUCRole;
-import org.jivesoftware.openfire.muc.MUCRoom;
-import org.jivesoftware.openfire.muc.MultiUserChatManager;
-import org.jivesoftware.openfire.muc.MultiUserChatService;
-import org.xmpp.packet.JID;
 import org.xmpp.packet.Message;
 
 public class MessageHook {
 
     private static MessageHook mHook;
     private static String prefix = ":!";
-    private MultiUserChatManager mMultiUserChatManager;
-    private MessageRouter mRouter;
 
     private MessageHook() {
-        mMultiUserChatManager = XMPPServer.getInstance()
-                .getMultiUserChatManager();
-        mRouter = XMPPServer.getInstance().getMessageRouter();
     }
 
     public static synchronized MessageHook getInstance() {
@@ -36,7 +27,6 @@ public class MessageHook {
 
     public void processMessage(final Message message) {
         String body = message.getBody();
-        final JID roomID = new JID("room1@conference.sheimi.vm");
         if (!body.startsWith(prefix))
             return;
         final String cmd = body.substring(prefix.length()).trim();
@@ -45,7 +35,7 @@ public class MessageHook {
             @Override
             public void run() {
                 System.out.println("Command " + cmd + " will process ... ");
-                StringBuilder s = new StringBuilder();
+                StringBuffer sb = new StringBuffer();
                 Process p;
                 try {
                     p = Runtime.getRuntime().exec(cmd);
@@ -54,32 +44,19 @@ public class MessageHook {
                     BufferedReader error = new BufferedReader(
                             new InputStreamReader(p.getErrorStream()));
                     String line = null;
-                    s.append("STDOUT:\n");
+                    sb.append("STDOUT:\n");
                     while ((line = input.readLine()) != null) {
-                        s.append(line);
-                        s.append('\n');
+                        sb.append(line);
+                        sb.append('\n');
                     }
-                    s.append("STDERR:\n");
+                    sb.append("STDERR:\n");
                     while ((line = error.readLine()) != null) {
-                        s.append(line);
-                        s.append('\n');
-                    }
-                    MultiUserChatService service = mMultiUserChatManager
-                            .getMultiUserChatService(roomID);
-                    MUCRoom room = service.getChatRoom(roomID.getNode());
-                    Collection<MUCRole> c = room.getOccupants();
-                    JID jid = new JID("room1@conference.sheimi.vm/Boardcast");
-                    for (MUCRole role : c) {
-                        JID to = role.getUserAddress();
-                        Message newMessage = message.createCopy();
-                        newMessage.setBody(s.toString());
-                        newMessage.setTo(to);
-                        newMessage.setFrom(jid);
-                        mRouter.route(newMessage);
-                        System.out.println(to);
+                        sb.append(line);
+                        sb.append('\n');
                     }
                     input.close();
                     error.close();
+                    MessageHookPlugin.broadCastToClient(sb.toString());
                 } catch (IOException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
--- src/java/me/sheimi/hackathon/MessageHookPlugin.java
+++ src/java/me/sheimi/hackathon/MessageHookPlugin.java
@@ -1,22 +1,43 @@
 package me.sheimi.hackathon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collection;
 
+import org.jivesoftware.openfire.MessageRouter;
+import org.jivesoftware.openfire.XMPPServer;
 import org.jivesoftware.openfire.container.Plugin;
 import org.jivesoftware.openfire.container.PluginManager;
 import org.jivesoftware.openfire.interceptor.InterceptorManager;
 import org.jivesoftware.openfire.interceptor.PacketInterceptor;
 import org.jivesoftware.openfire.interceptor.PacketRejectedException;
+import org.jivesoftware.openfire.muc.MUCRole;
+import org.jivesoftware.openfire.muc.MUCRoom;
+import org.jivesoftware.openfire.muc.MultiUserChatManager;
+import org.jivesoftware.openfire.muc.MultiUserChatService;
 import org.jivesoftware.openfire.session.Session;
+import org.xmpp.packet.JID;
 import org.xmpp.packet.Message;
 import org.xmpp.packet.Packet;
 
-public class MessageHookPlugin implements Plugin, PacketInterceptor {
+public class MessageHookPlugin implements Plugin, PacketInterceptor, Runnable {
 
     private InterceptorManager mInterceptorManager;
+    private MultiUserChatManager mMultiUserChatManager;
+    private MessageRouter mRouter;
 
     public MessageHookPlugin() {
         mInterceptorManager = InterceptorManager.getInstance();
+        mMultiUserChatManager = XMPPServer.getInstance()
+                .getMultiUserChatManager();
+        mRouter = XMPPServer.getInstance().getMessageRouter();
     }
 
     @Override
@@ -42,11 +63,92 @@ public class MessageHookPlugin implements Plugin, PacketInterceptor {
     @Override
     public void initializePlugin(PluginManager manager, File pluginDirectory) {
         mInterceptorManager.addInterceptor(this);
+        createFIFOServer();
     }
 
     @Override
     public void destroyPlugin() {
+        stopFIFOServer();
         mInterceptorManager.removeInterceptor(this);
+    }
+
+    public static final String TMP_DIR = "/tmp/message-hook/";
+    public static final String FIFO_SERVER = TMP_DIR + "chatroom-broadcast";
+
+    private void createFIFOServer() {
+        File tmpDir = new File(TMP_DIR);
+        if (!tmpDir.exists())
+            tmpDir.mkdirs();
+        try {
+            Runtime.getRuntime().exec("mkfifo " + FIFO_SERVER);
+            Thread thread = new Thread(this);
+            thread.start();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void stopFIFOServer() {
+        File fifo = new File(FIFO_SERVER);
+        fifo.delete();
+    }
+
+    @Override
+    public void run() {
+        File fifo = new File(FIFO_SERVER);
+        JID roomID = new JID("room1@conference.sheimi.vm");
+        while (true) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        new FileInputStream(fifo)));
+                StringBuilder s = new StringBuilder();
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    s.append(line);
+                    s.append('\n');
+                }
+                br.close();
+
+                MultiUserChatService service = mMultiUserChatManager
+                        .getMultiUserChatService(roomID);
+                MUCRoom room = service.getChatRoom(roomID.getNode());
+                Collection<MUCRole> c = room.getOccupants();
+                JID jid = new JID("room1@conference.sheimi.vm/Boardcast");
+                for (MUCRole role : c) {
+                    JID to = role.getUserAddress();
+                    Message newMessage = new Message();
+                    newMessage.setType(Message.Type.groupchat);
+                    newMessage.setBody(s.toString().trim());
+                    newMessage.setTo(to);
+                    newMessage.setFrom(jid);
+                    mRouter.route(newMessage);
+                }
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void broadCastToClient(String content) {
+        File fifo = new File(MessageHookPlugin.FIFO_SERVER);
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream(fifo)));
+            bw.write(content);
+            bw.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
